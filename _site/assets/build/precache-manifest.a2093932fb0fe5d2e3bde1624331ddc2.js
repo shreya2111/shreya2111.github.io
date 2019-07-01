8e0486ff27f0aa3a268b36000bbc33add18c7a4c
Shreya <shreya.cogsci@gmail.com>
2019-07-01 12:46:56 -0700

    adding two blog posts

---
+++ _site/assets/build/precache-manifest.a2093932fb0fe5d2e3bde1624331ddc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b42c48224a74f33970882061d0f6b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dca7c23138e8dc1b788",
+    "url": "/static/css/main.c527181f.chunk.css"
+  },
+  {
+    "revision": "4dca7c23138e8dc1b788",
+    "url": "/static/js/main.dcb768f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
